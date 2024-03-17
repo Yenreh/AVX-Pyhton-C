23fecc56503269cd2c6e4798aa447cfa14b1ad89
yenreh <qherney@gmail.com>
2024-03-17 16:53:58 -0500

    General Update create a function for scalar multiplication for cprofiler

--- src/cprofile_numpy.py
+++ src/cprofile_numpy.py
@@ -2,15 +2,16 @@ import numpy as np
 import cProfile as profile
 import pstats
 
-prof = profile.Profile()
 
-# Definition of constants
-n = 10000000
-lst = range(0, n)
-x = 2
+prof = profile.Profile()
 
 
 def scalar_multiplication():
+    # Definition of constants
+    n = 10000000
+    lst = range(0, n)
+    x = 2
+
     # Vector creation using numpy
     v = np.array(lst)
     # Scalar multiplication
--- src/time_numpy.py
+++ src/time_numpy.py
@@ -1,20 +1,26 @@
 import numpy as np
 import timeit
 
+
+def scalar_multiplication():
+    # Definition of constants
+    n = 10000000
+    lst = range(0, n)
+    x = 2
+
+    # Vector creation using numpy
+    v = np.array(lst)
+
+    # Scalar multiplication
+    for i in lst:
+        v[i] = v[i] * x
+
+
 # Start measuring time
 starting_time = timeit.default_timer()
 
-# Definition of constants
-n = 10000000
-lst = range(0, n)
-x = 2
-
-# Vector creation using numpy
-v = np.array(lst)
-
-# Scalar multiplication
-for i in lst:
-    v[i] = v[i] * x
+# Function call
+scalar_multiplication()
 
 # End measuring time
 ending_time = timeit.default_timer()
